0bccf8a10a6e6d9d0da25e15b3130916352f3d51
kienhs <kienhs@dolphinsolutions.vn>
2023-07-20 16:06:58 +0700

    Add database

---
+++ database/migrations/2023_07_20_090253_create_crons_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCronsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('crons', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('date');
+            $table->integer('total');
+            $table->integer('current');
+            $table->tinyInteger('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('crons');
+    }
+}
---
+++ database/migrations/2023_07_20_090305_create_cron_detail_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCronDetailTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cron_detail', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('cron_id')->index();
+            $table->foreign('cron_id')->references('id')->on('crons')->onDelete('cascade');
+            $table->json('payload');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cron_detail');
+    }
+}
