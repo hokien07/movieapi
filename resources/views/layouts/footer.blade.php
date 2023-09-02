34ac1c1f427c0b79c339b218a1f9a1750f0e8867
kienhs <kienhs@dolphinsolutions.vn>
2023-09-02 11:33:03 +0700

    Add google tag

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -27,3 +27,12 @@
         });
     });
 </script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BVD4D1VNTV"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-BVD4D1VNTV');
+</script>
