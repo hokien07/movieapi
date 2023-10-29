7b1ccb9d770e444b62e61f3b0a7a4a982bd1cc2b
kienhs <kienhs@dolphinsolutions.vn>
2023-10-29 16:54:15 +0700

    Apply cache

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -113,19 +113,6 @@
                 </div>
             </div>
             <div class="bixbox">
-                <div class="releases"><h3><span>Bình luận</span></h3></div>
-                <div class="cmt commentx">
-                    <div class="box-comment" id="tabs-facebook"
-                         style="background: linear-gradient(to right, #ffff, #ffff);">
-                        <div class="fb-comments" data-href="{{route('movie', $movie->slug)}}"
-                             data-width="100%"
-                             data-numposts="10" data-order-by="reverse_time" data-colorscheme="light">
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="bixbox">
                 <div class="releases"><h3>Phim cùng thể loại</h3></div>
                 <div class="series-gen">
                     <div class="listupd">
